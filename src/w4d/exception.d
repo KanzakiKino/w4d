68153ce1efc23b2af29c0b18ef96244fd43feec1
KanzakiKino <kino@knzk.work>
2018-06-30 09:42:54 +0900

    [add] Added alias of std.exception.enforce.

--- src/w4d/exception.d
+++ src/w4d/exception.d
@@ -1,10 +1,13 @@
 // Written under LGPL-3.0 in the D programming language.
 // Copyright 2018 KanzakiKino
 module w4d.exception;
+static import std.exception;
+
+alias enforce(T=W4dException) = std.exception.enforce!T;
 
 class W4dException : Exception
 {
-    this ( string mes, string file = __FILE__, int line = __LINE__ )
+    this ( string mes, string file = __FILE__, size_t line = __LINE__ )
     {
         super( mes, file, line );
     }
