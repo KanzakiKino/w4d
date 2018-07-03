15b744e5f407d2b6a1b9ab9fae5251407b0d07ca
KanzakiKino <kino@knzk.work>
2018-07-04 08:46:07 +0900

    [update] Added Rect.

---
+++ src/w4d/style/rect.d
@@ -0,0 +1,74 @@
+// Written under LGPL-3.0 in the D programming language.
+// Copyright 2018 KanzakiKino
+module w4d.style.rect;
+import w4d.style.exception,
+       w4d.style.scalar;
+import g4d.math.vector;
+
+unittest
+{
+    auto rc = Rect( Scalar(50f,ScalarUnit.Percent) );
+    rc.calc( vec2(300f,200f) );
+    assert( rc.isRelative );
+    assert( rc.isCalced );
+    assert( rc.top   .calced == 100f );
+    assert( rc.right .calced == 150f );
+    assert( rc.bottom.calced == 100f );
+    assert( rc.left  .calced == 150f );
+}
+
+struct Rect
+{
+    Scalar top, right, bottom, left;
+
+    this ( Scalar[] args... )
+    {
+        if ( args.length == 1 ) {
+            top    = args[0];
+            right  = args[0];
+            bottom = args[0];
+            left   = args[0];
+        } else if ( args.length == 2 ) {
+            top    = args[0];
+            right  = args[1];
+            bottom = args[0];
+            left   = args[1];
+        } else if ( args.length == 3 ) {
+            top    = args[0];
+            right  = args[1];
+            bottom = args[2];
+            left   = args[1];
+        } else if ( args.length == 4 ) {
+            top    = args[0];
+            right  = args[1];
+            bottom = args[2];
+            left   = args[3];
+        } else {
+            throw new StyleException( "Args length is too many to initialize Rect." );
+        }
+    }
+
+    @property isAbsolute ()
+    {
+        return top.isAbsolute && right.isAbsolute && bottom.isAbsolute && left.isAbsolute;
+    }
+    @property isRelative ()
+    {
+        return !isAbsolute;
+    }
+    @property isCalced ()
+    {
+        return top.isCalced && right.isCalced && bottom.isCalced && left.isCalced;
+    }
+
+    void calc ( vec2 parentSize, vec2 def = vec2(0,0) )
+    {
+        const xBase = ScalarUnitBase( def.x, parentSize.x );
+        const yBase = ScalarUnitBase( def.y, parentSize.y );
+
+        top   .calc( yBase );
+        right .calc( xBase );
+        bottom.calc( yBase );
+        left  .calc( xBase );
+    }
+}
