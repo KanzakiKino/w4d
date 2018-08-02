005b38e0ae144b4c94c01da6f83a0d9610a08863
KanzakiKino <kino@knzk.work>
2018-08-02 18:16:30 +0900

    [add] Added PopupDialogWidget.

--- src/test.d
+++ src/test.d
@@ -37,6 +37,7 @@ class TestRootWidget : RootWidget
 
         _tooltip = new PopupTooltipWidget;
         _tooltip.loadText( "Right click to open tree."d, fontface );
+        _context.setPopup( new PopupDialogWidget );
 
         _menu = new MenuPopupWidget;
         foreach ( i; 0..5 ) {
---
+++ src/w4d/widget/popup/dialog/base.d
@@ -0,0 +1,48 @@
+// Written under LGPL-3.0 in the D programming language.
+// Copyright 2018 KanzakiKino
+module w4d.widget.popup.dialog.base;
+import w4d.parser.theme,
+       w4d.style.rect,
+       w4d.style.scalar,
+       w4d.widget.popup.base,
+       w4d.widget.base,
+       w4d.exception;
+import g4d.math.vector;
+
+class PopupDialogWidget : PopupWidget
+{
+    override void handlePopup ( bool opened, WindowContext w )
+    {
+        if ( opened ) {
+            requestLayout();
+        }
+        super.handlePopup( opened, w );
+    }
+
+    this ()
+    {
+        super();
+
+        parseThemeFromFile!"theme/tooltip.yaml"( style );
+        style.box.borderWidth = Rect(1.pixel);
+        style.box.paddings    = Rect(2.mm);
+    }
+
+    override void move ( vec2, vec2 )
+    {
+        throw new W4dException( "Cannot move the dialog." );
+    }
+
+    override vec2 layout ( vec2 pos, vec2 parentSize )
+    {
+        auto size = .Widget.layout( pos, parentSize );
+        auto late = (parentSize-size)/2 + pos;
+        late     -= style.translate;
+
+        style.x.alter( style.x.calced + late.x );
+        style.y.alter( style.y.calced + late.y );
+        shiftChildren( late - style.translate );
+
+        return vec2(0,0);
+    }
+}
