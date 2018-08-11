f1a5eef04d7775c9ed0cb2b3c720accece0a47a1
KanzakiKino <kino@knzk.work>
2018-08-11 11:41:49 +0900

    [update] Updated test code.

--- src/test.d
+++ src/test.d
@@ -1,7 +1,8 @@
 // Written without LICENSE in the D programming language.
 // Copyright 2018 KanzakiKino
 import w4d;
-import std.conv;
+import std.conv,
+       std.format;
 
 class TestRootWidget : RootWidget
 {
@@ -13,15 +14,86 @@ class TestRootWidget : RootWidget
 
         auto fontface = new FontFace(new Font("/usr/share/fonts/TTF/Ricty-Regular.ttf"), vec2i(16,16));
 
+        auto btn = new ButtonWidget;
+        btn.loadText( "add"d, fontface );
+        btn.style.box.size.width = Scalar.None;
+        addChild( btn );
+
         auto host = new MdiHostWidget;
         addChild( host );
 
-        foreach ( i; 0..5 ) {
+        btn.onButtonPress = () {
             auto cli1 = new MdiClientWidget;
-            cli1.limitSize( vec2(200,200), vec2(400,400) );
+            cli1.limitSize( vec2(400,400), vec2(600,600) );
+            cli1.resize( vec2(500,500) );
             cli1.loadText( "Client1 - MdiClientWidget"d, fontface );
+            cli1.contents.addChild( createInputPage( fontface ) );
             host.addClient( cli1 );
+        };
+    }
+
+    protected Widget createInputPage ( FontFace face )
+    {
+        auto scroll = new VerticalScrollPanelWidget;
+
+        auto page = scroll.contents;
+
+        foreach ( c; 0..30 ) {
+            {
+                auto line = new PanelWidget;
+                line.setLayout!HorizontalLineupLayout;
+                page.addChild( line );
+
+                auto checkbox1 = new CheckBoxWidget;
+                checkbox1.style.box.margins = Rect(1.mm); // TODO
+                checkbox1.loadText( "CheckBoxWidget"d, face );
+                line.addChild( checkbox1 );
+
+                auto checkbox2 = new CheckBoxWidget;
+                checkbox2.style.box.margins = Rect(1.mm); // TODO
+                checkbox2.loadText( "CheckBoxWidget"d, face );
+                line.addChild( checkbox2 );
+            }
+            {
+                auto line = new PanelWidget;
+                line.setLayout!HorizontalMonospacedSplitLayout;
+                page.addChild( line );
+
+                auto input1 = new LineInputWidget;
+                input1.style.box.margins = Rect(1.mm); // TODO
+                input1.loadText( "normal - LineInputWidget"d, face );
+                line.addChild( input1 );
+
+                auto input2 = new LineInputWidget;
+                input2.style.box.margins = Rect(1.mm); // TODO
+                input2.loadText( "locked - LineInputWidget"d, face );
+                input2.lock();
+                line.addChild( input2 );
+            }
+            {
+                auto line = new PanelWidget;
+                line.setLayout!HorizontalSplitLayout;
+                page.addChild( line );
+
+                auto select = new SelectInputWidget;
+                foreach ( i; 0..5 ) {
+                    auto item = new MenuItemWidget;
+                    select.addItem( item );
+                    auto text = new TextWidget;
+                    item.setChild( text );
+
+                    if ( i == 0 ) {
+                        select.select( item );
+                    }
+                    text.loadText( "item to be selected (%d)"d.format(i), face );
+                }
+                line.addChild( select );
+
+                auto slider = new HorizontalSliderWidget;
+                line.addChild( slider );
+            }
         }
+        return scroll;
     }
 }
 
